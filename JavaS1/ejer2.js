aba6ff6f35996deb1719f67906b4e10b23de03eb
Jces00 <84345750+Jces00@users.noreply.github.com>
2023-05-22 12:04:13 -0600

    Update ejer2.js

--- JavaS1/ejer2.js
+++ JavaS1/ejer2.js
@@ -4,8 +4,7 @@
     //Comprobamos que tenga formato correcto
     var Fecha_aux = document.getElementById("Fecha").value.split("/");
      var Fecha1 = new Date(parseInt(fecha_aux[2]),parseInt(fecha_aux[1]-1),parseInt(fecha_aux[0]));
-   
-     //Comprobamos si existe la fecha
+    //Comprobamos si existe la fecha
     if (isNaN(Fecha1)){
       alert("Fecha introducida incorrecta");
       return false;
